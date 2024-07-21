85f7ccfd1965092a0d82fcb849763b646ed918f9
TLP-247 <nitzan.tal5@mail.huji.ac.il>
2024-07-22 00:27:24 +0300

    num_frames

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 
 
-
 void print_bytes(uint64_t virtualAddress) {
     for (int i = VIRTUAL_ADDRESS_WIDTH-1; i >= 0; --i) {
         uint64_t bit = (virtualAddress >> i) & 1;
@@ -87,6 +86,42 @@ word_t get_frame_dfs(word_t frame_not_to_evict, word_t curr_frame_index, int hei
     return found_frame_index;
 }
 
+
+void get_frame_max_point(word_t frame_not_to_evict, word_t curr_frame_index, uint64_t page_swapped_in, uint64_t curr_p,word_t *frame_res, uint64_t *res_p, uint64_t *max_val, uint64_t *father, uint64_t *father_address_res, int offset, int height)
+{
+    if (curr_frame_index == frame_not_to_evict)
+    {
+        return;
+    }
+    else if (height == TABLES_DEPTH - 1)
+    {
+        *father = curr_frame_index*PAGE_SIZE + offset;
+    }
+    else if (height == TABLES_DEPTH)
+    {
+        uint64_t res = page_swapped_in > curr_p ? page_swapped_in-curr_p : curr_p - page_swapped_in;
+        uint64_t tot_res = NUM_PAGES - res < res ? NUM_PAGES - res : res;
+        if (tot_res > *max_val)
+        {
+            *res_p = curr_p;
+            *max_val = tot_res;
+            *frame_res = curr_frame_index;
+            *father_address_res = *father;
+        }
+        return;
+    }
+
+    word_t pointing_index;
+    word_t found_frame_index = 0;
+    for (int i = 0; i < PAGE_SIZE; i++)
+    {
+        curr_p = (curr_p << OFFSET_WIDTH) + i;
+        PMread(curr_frame_index*PAGE_SIZE + i, &pointing_index);
+        get_frame_max_point(frame_not_to_evict, pointing_index, page_swapped_in, curr_p, frame_res, res_p, max_val, father, father_address_res, i, height + 1);
+    }
+}
+
+
 int get_num_of_frames(word_t curr_frame_index, int height)
 {
     if (height == TABLES_DEPTH)
@@ -98,6 +133,7 @@ int get_num_of_frames(word_t curr_frame_index, int height)
     int total_num = 0;
     for (int i = 0; i < PAGE_SIZE; i++)
     {
+        get_page_index(curr_frame_index*PAGE_SIZE + i);
         PMread(curr_frame_index*PAGE_SIZE + i, &pointing_index);
         total_num += get_num_of_frames(pointing_index, height + 1);
     }
@@ -142,6 +178,26 @@ word_t get_frame(int is_next_data, uint64_t page_index, word_t frame_not_to_evic
         return found_frame;
     }
 
+    uint64_t max_val = 0;
+    word_t frame_res;
+    uint64_t father_res;
+    uint64_t father;
+    uint64_t res_p = 0;
+    get_frame_max_point(frame_not_to_evict, 0, page_index, 0, &frame_res, &res_p, &max_val, &father, &father_res, 0, 0);
+    PMevict(frame_res, res_p);
+
+
+    PMwrite(father_res, 0); //writing 0 to father
+    if (is_next_data)
+    {
+        PMrestore(frame_res, page_index);
+    }
+    else
+    {
+        put_in_frame_zeros(frame_res);
+    }
+
+    return frame_res;
 }
 
 word_t get_physical_page(uint64_t virtualAddress){
@@ -183,16 +239,16 @@ int VMread(uint64_t virtualAddress, word_t* value){
 }
 
 
-int main(int argc, char **argv) {
-    uint64_t n1 = 100 + (uint64_t)(1ULL << 18);
-    print_bytes(n1);
-    std:: cout << "Table depth " << TABLES_DEPTH << std::endl;
-    std:: cout << "without offset" << get_page_index (n1) << std::endl;
-    std:: cout << "table_index 0 of" << n1 << ": " << get_table_value(n1, 0) << std::endl;
-    std:: cout << "table_index 1 of" << n1 << ": " << get_table_value(n1, 1) << std::endl;
-    std:: cout << "table_index 2 of" << n1 << ": " << get_table_value(n1, 2) << std::endl;
-    std:: cout << "table_index 3 of" << n1 << ": " << get_table_value(n1, 3) << std::endl;
-    std:: cout << "table_index 4 of" << n1 << ": " << get_table_value(n1, 4) << std::endl;
-
-    return 0;
-}
+//int main(int argc, char **argv) {
+//    uint64_t n1 = 100 + (uint64_t)(1ULL << 18);
+//    print_bytes(n1);
+//    std:: cout << "Table depth " << TABLES_DEPTH << std::endl;
+//    std:: cout << "without offset" << get_page_index (n1) << std::endl;
+//    std:: cout << "table_index 0 of" << n1 << ": " << get_table_value(n1, 0) << std::endl;
+//    std:: cout << "table_index 1 of" << n1 << ": " << get_table_value(n1, 1) << std::endl;
+//    std:: cout << "table_index 2 of" << n1 << ": " << get_table_value(n1, 2) << std::endl;
+//    std:: cout << "table_index 3 of" << n1 << ": " << get_table_value(n1, 3) << std::endl;
+//    std:: cout << "table_index 4 of" << n1 << ": " << get_table_value(n1, 4) << std::endl;
+//
+//    return 0;
+//}
