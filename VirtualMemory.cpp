68d7495c31e3bdf5d073f0765106f228ecf68618
TLP-247 <nitzan.tal5@mail.huji.ac.il>
2024-07-21 12:57:15 +0300

    table template

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,6 +2,12 @@
 #include "MemoryConstants.h"
 #include "PhysicalMemory.h"
 
+uint64_t get_table_value(uint64_t virtualAddress, int table_index)
+{
+
+}
+
+
 void VMinitialize()
 {
     for(int i =0; i < PAGE_SIZE; i++)
